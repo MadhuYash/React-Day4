1b867c81753a0cad9c7a6cfc2da0dafded76ab92
MadhuYash <118814066+MadhuYash@users.noreply.github.com>
2023-02-07 21:51:30 +0530

    React Day4 Assignment

---
+++ React day 4 assignment/React day 4 assignment/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import './App.css';
+import ClassLifeCycle from './components/assignment/ClassLifeCycle';
+import FunctionalUseEffect from './components/assignment/FunctionalUseEffect';
+import RenderEmployee from "./components/RenderEmployee";
+import UseEffectComponent from "./components/UseEffectComponent";
+
+
+function App() {
+  return (
+    <div className="App">
+      
+         <ClassLifeCycle/>
+         <FunctionalUseEffect/>
+       
+       {/* <RenderEmployee/> */}
+       {/* <UseEffectComponent/> */}
+       
+  
+     
+    </div>
+  );
+}
+export default App;
